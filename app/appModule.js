34fc435e238cad98e76e5a552e1551771ba33d08
jaudas <jessica.audas@epfedu.Fr>
2015-04-20 11:35:04 +0200

    header

--- app/appModule.js
+++ app/appModule.js
@@ -1,4 +1,40 @@
-var Stations = angular.module('Stations', []);
+var Stations = angular.module('Stations', ['ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute',
+    'google-maps'])
+
+
+
+;
+
+Stations.config(functioNn ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: '../index.html',
+        controller: ''
+      })
+      .when('/itineraire', {
+        templateUrl: 'views/itin.html',
+        controller: ''
+      })
+      .when('/stations', {
+        templateUrl: 'views/stations.html',
+        controller: 'StationCtrl'
+      })
+      .when('/info', {
+        templateUrl: 'views/hotel.html',
+        controller: ''
+      })
+      .when('/statistiques', {
+        templateUrl: 'views/findHotel.html',
+        controller: ''
+      })
+      .otherwise({
+        templateUrl: 'views/404.html',
+        controller: ''
+      });
+});
 
 
 Stations.filter('ago',function(){
